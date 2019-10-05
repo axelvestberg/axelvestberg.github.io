69e8dace3c15537eb02b46feee7a2f43a834d09b
axelvestberg <axel.vestberg@gmail.com>
2019-10-05 14:15:14 +0200

    Updates

---
+++ precache-manifest.3367eda02ad87c0d499e906f9e5d0752.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0195ce9db67979d46b13aecde562d6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8c280d1275e7d639683",
+    "url": "/static/css/main.daca6879.chunk.css"
+  },
+  {
+    "revision": "ab9e67dc87353bd0bc43",
+    "url": "/static/js/2.41d59747.chunk.js"
+  },
+  {
+    "revision": "c8c280d1275e7d639683",
+    "url": "/static/js/main.cd03276b.chunk.js"
+  },
+  {
+    "revision": "c10c9bfb23790dcfcd84",
+    "url": "/static/js/runtime-main.08353d53.js"
+  }
+]);
